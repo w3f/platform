bc86c77bd1e37a60a5e8bc6bb7a16bca0246af14
Federico Gimenez <federico.gimenez@gmail.com>
2019-06-03 18:37:34 +0200

    added community module

---
+++ modules/community/cluster.tf
@@ -0,0 +1,11 @@
+resource "digitalocean_kubernetes_cluster" "w3f" {
+  name    = "w3f"
+  region  = "${var.region}"
+  version = "${var.k8s_version}"
+
+  node_pool {
+    name       = "node-pool"
+    size       = "${var.node_size}"
+    node_count = "${var.node_count}"
+  }
+}
---
+++ modules/community/outputs.tf
@@ -0,0 +1,4 @@
+output "kubeconfig" {
+  value = "${digitalocean_kubernetes_cluster.w3f.kube_config.0.raw_config}"
+  sensitive = true
+}
