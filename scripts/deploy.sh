d677d0e143e943a02e694f3225e4066cf24a79d8
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-16 16:57:01 +0200

    updates for development cluster

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 set -xe
 
-if [ "${DEPLOYMENT}" = w3f ]; then
-    NODE_COUNT=4
-else
-    NODE_COUNT=3
-fi
-
 cd ./modules/${DEPLOYMENT}
-terraform init \
-          -backend-config="access_key=$SPACES_ACCESS_TOKEN" \
-          -backend-config="secret_key=$SPACES_SECRET_KEY" \
-          -backend-config="bucket=$SPACES_BUCKET_NAME" \
-          -backend-config="endpoint=$SPACES_ENDPOINT"
 
-terraform apply -auto-approve -var node_count=${NODE_COUNT}
+if [ "${DEPLOYMENT}" = development ]; then
+    echo "${GOOGLE_APPLICATION_CREDENTIALS_CONTENT}" | tee credentials.json
+    export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/credentials.json"
 
-terraform output kubeconfig &> kubeconfig.yaml
-export KUBECONFIG=$(pwd)/kubeconfig.yaml
+    terraform init
 
-if [ ! $(kubectl get sa -n kube-system | grep tiller) ]; then
-    kubectl -n kube-system create sa tiller
+    terraform apply -auto-approve
+else
+    terraform init \
+              -backend-config="access_key=$SPACES_ACCESS_TOKEN" \
+              -backend-config="secret_key=$SPACES_SECRET_KEY" \
+              -backend-config="bucket=$SPACES_BUCKET_NAME" \
+              -backend-config="endpoint=$SPACES_ENDPOINT"
+
+    terraform apply -auto-approve
+
+    terraform output kubeconfig &> kubeconfig.yaml
+    export KUBECONFIG=$(pwd)/kubeconfig.yaml
+
+    if [ ! $(kubectl get sa -n kube-system | grep tiller) ]; then
+        kubectl -n kube-system create sa tiller
+    fi
+
+    if [ ! $(kubectl get clusterrolebinding | grep tiller) ]; then
+        kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+    fi
+
+    helm init --service-account tiller --history-max=5
+
+    helm upgrade --install --namespace kube-system -f metrics-server-values.yaml metrics stable/metrics-server
 fi
-
-if [ ! $(kubectl get clusterrolebinding | grep tiller) ]; then
-    kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-fi
-
-helm init --service-account tiller --history-max=5
-
-helm upgrade --install --namespace kube-system -f metrics-server-values.yaml metrics stable/metrics-server
