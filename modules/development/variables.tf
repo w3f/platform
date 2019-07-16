a18e3b402cb61169d123ac4a21fc544effdc1ea1
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-16 16:56:36 +0200

    development cluster

---
+++ modules/development/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  default = "development"
+}
+
+variable "location" {
+  default = "europe-west6-a"
+}
+
+variable "node_count" {
+  default = 2
+}
+
+variable "machine_type" {
+  default = "n1-standard-2"
+}
+
+variable "k8s_version" {
+  default = "1.13.6-gke.13"
+}
