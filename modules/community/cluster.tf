97e9c5452ffdfdced107467cc24e9b9e8908a53d
Federico Gimenez <federico.gimenez@gmail.com>
2019-09-03 12:11:20 +0200

    bump terraform version

--- modules/community/cluster.tf
+++ modules/community/cluster.tf
@@ -1,11 +1,11 @@
 resource "digitalocean_kubernetes_cluster" "do_cluster" {
-  name    = "${var.name}"
-  region  = "${var.region}"
-  version = "${var.k8s_version}"
+  name    = var.name
+  region  = var.region
+  version = var.k8s_version
 
   node_pool {
     name       = "node-pool"
-    size       = "${var.node_size}"
-    node_count = "${var.node_count}"
+    size       = var.node_size
+    node_count = var.node_count
   }
 }
