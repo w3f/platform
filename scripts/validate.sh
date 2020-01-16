f278db567d30fef61e707d242ece864dc3947388
Federico Gimenez <federico.gimenez@gmail.com>
2020-01-16 10:37:20 +0100

    fix do validate

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -6,8 +6,14 @@ cd ./modules/${DEPLOYMENT}
 if [ "${DEPLOYMENT}" = development ] || [ "${DEPLOYMENT}" = development-runners ] || [ "${DEPLOYMENT}" = engineering ]; then
     echo "${GOOGLE_APPLICATION_CREDENTIALS_CONTENT}" | tee credentials.json
     export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/credentials.json"
+
+    terraform init
+else
+    terraform init -backend-config="access_key=$SPACES_ACCESS_TOKEN" \
+              -backend-config="secret_key=$SPACES_SECRET_KEY" \
+              -backend-config="bucket=$SPACES_BUCKET_NAME" \
+              -backend-config="endpoint=$SPACES_ENDPOINT"
 fi
 
-terraform init
 terraform validate
 terraform plan
