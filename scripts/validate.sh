eeee779e10972c1f536f3bbf94c08b19917adae6
Federico Gimenez <federico.gimenez@gmail.com>
2020-01-16 11:50:48 +0100

    address review comments

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -4,7 +4,7 @@ set -xe
 cd ./modules/${DEPLOYMENT}
 
 if [ "${DEPLOYMENT}" = development ] || [ "${DEPLOYMENT}" = development-runners ] || [ "${DEPLOYMENT}" = engineering ]; then
-    echo "${GOOGLE_APPLICATION_CREDENTIALS_CONTENT}" | tee credentials.json
+    echo -n "${GOOGLE_APPLICATION_CREDENTIALS_CONTENT}" > credentials.json
     export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/credentials.json"
 
     terraform init
