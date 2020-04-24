16d81d3b933cf15dd87d7e61c7f72a3bedea068d
Federico Gimenez <federico.gimenez@gmail.com>
2020-04-24 12:38:29 +0200

    azure credentials

--- modules/china/main.tf
+++ modules/china/main.tf
@@ -5,4 +5,9 @@ module "polkadot-k8s" {
   cluster_name = "w3f-china"
   node_count   = 3
   location     = "eastasia"
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
